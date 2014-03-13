ee2f47c7bfe8ffbd179567d4cb9909bb099c5417
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-03-13 02:05:49 +0100

    Reformatted indentation
    Removed all spaces and added tabs instead.

--- src/main.c
+++ src/main.c
@@ -18,78 +18,77 @@
 #include "commands.h"
 
 int main(int argc, char** argv)
-{    
-    
+{
     unsigned short port = LISTEN_PORT;
     struct in_addr addr;
     addr.s_addr = LISTEN_ADDRESS;
-    
+
     /* Command line options */
     // _BSD_SOURCE required for inet_aton()
     // getopt() needs _POSIX_C_SOURCE >= 2 or _XOPEN_SOURCE
-    
+
 #if defined _BSD_SOURCE && (_POSIX_C_SOURCE >= 2 || defined _XOPEN_SOURCE)
     int opt;
     while ((opt = getopt(argc, argv, "va:p:")) != -1)
     {
         switch(opt)
         {
-            case 'v': /* Verbose */
-                debug_set_verbose(1);
-                break;
-            case 'a': /* Address */
-                if (inet_aton(optarg, &addr) == 0)
-                {
-                    fprintf(stderr, "%s: invalid address -- '%s'\n", argv[0], optarg);
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case '?':
-                fprintf(stderr, "%s: invalid option -- '%c'\n", argv[0], optopt);
-                fprintf(stderr, "Usage: %s [-v] [-a addr] [-p port]\n", argv[0]);
+        case 'v': /* Verbose */
+            debug_set_verbose(1);
+            break;
+        case 'a': /* Address */
+            if (inet_aton(optarg, &addr) == 0)
+            {
+                fprintf(stderr, "%s: invalid address -- '%s'\n", argv[0], optarg);
                 exit(EXIT_FAILURE);
-            default:
-                error_print_exit("getopt");
+            }
+            break;
+        case 'p':
+            port = atoi(optarg);
+            break;
+        case '?':
+            fprintf(stderr, "%s: invalid option -- '%c'\n", argv[0], optopt);
+            fprintf(stderr, "Usage: %s [-v] [-a addr] [-p port]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        default:
+            error_print_exit("getopt");
         }
     }
 #else
 #warning "getopt not available. Will use default values."
 #warning "getopt requires _BSD_SOURCE && (_POSIX_C_SOURCE >= 2 || _XOPEN_SOURCE)" 
 #endif
-    
+
     info_print_format("Starting server at port %d", port);
     int serverfd = socket_create_and_bind(addr.s_addr, port);
     if (serverfd < 0)
     {
         error_print_exit("socket_create_and_bind");
     }
-    
+
     info_print("Setting server socket to nonblocking mode");
     if (socket_set_nonblocking(serverfd) < 0)
     {
         error_print_exit("socket_set_nonblocking");
     }
-    
+
     info_print("Listening with max number of clients");
     if (socket_listen(serverfd, -1) < 0)
     {
         error_print_exit("socket_listen");
     }
-    
+
     info_print("Registering event handlers");
     event_register_handlers();
     info_print("Registering command handlers");
     command_register_handlers();
-    
+
     info_print("Registering signal handlers");
     signal_register_handlers();
-    
+
     info_print("Entering main event loop");
     event_start_loop(serverfd);
-    
+
     info_print("Shutdown complete");
     return EXIT_SUCCESS;
 }
